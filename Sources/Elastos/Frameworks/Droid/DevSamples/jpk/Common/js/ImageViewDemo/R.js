d4525f4fc458cd6bdbbfbcda75cd5f9ca6167cc0
wanghuaimin <gdsys@126.com>
2016-05-11 11:37:50 +0800

    add jpk common code to devsamples

---
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/ImageViewDemo/R.js
@@ -0,0 +1,20 @@
+module.exports = {
+    attr : {
+    },
+    drawable : {
+        earth : 0x7f020000,
+        icon : 0x7f020001,
+        transformer : 0x7f020002,
+        wunderland : 0x7f020003,
+    },
+    id : {
+        ImageView : 0x7f050000,
+    },
+    layout : {
+        main : 0x7f030000,
+        test_item : 0x7f030001,
+    },
+    string : {
+        app_name : 0x7f040000,
+    },
+};
