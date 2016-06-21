4128a975606fbd50dcf2768e1b538e11dab3888b
cao.jing <cao.jing@kortide.com>
2016-06-21 10:15:12 +0000

    update node_r

--- Sources/External/node_r/elastos/bridge/V8CarObject.cpp
+++ Sources/External/node_r/elastos/bridge/V8CarObject.cpp
@@ -50,5 +50,42 @@ v8::Local<v8::Object> V8CarObject::New(
     return value;
 }
 
+bool V8CarObject::IsInstance(
+    /* [in] */ v8::Local<v8::Object> object)
+{
+    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+    v8::Local<v8::FunctionTemplate> objectDesc =
+            v8::Local<v8::FunctionTemplate>::New(isolate, sCarObjectDesc);
+    return objectDesc->HasInstance(object);
+}
+
+AutoPtr<V8CarObject> V8CarObject::AsV8CarObject(
+    /* [in] */ v8::Local<v8::Object> object)
+{
+    assert(object->InternalFieldCount() >= 1);
+    V8CarObject* carObject = (V8CarObject*)object->GetAlignedPointerFromInternalField(sV8CarObjectIndex);
+    return carObject;
+}
+
+v8::Handle<v8::Value> V8CarObject::V8CarObjectGetProperty(
+    /* [in] */ v8::Local<v8::Object> self,
+    /* [in] */ const String& identifier,
+    /* [in] */ v8::Local<v8::Value> key)
+{
+    AutoPtr<V8CarObject> carObject = AsV8CarObject(self);
+
+}
+
+void V8CarObject::V8CarObjectNamedPropertyGetter(
+    /* [in] */ v8::Local<v8::String> property,
+    /* [in] */ const v8::PropertyCallbackInfo<v8::Value>& info)
+{
+    v8::String::Utf8Value utfStr(property);
+    String identifier(*utfStr);
+    v8::Handle<v8::Value> value = V8CarObjectGetProperty(info.Holder(), identifier, property);
+    info.GetReturnValue().Set(value);
+}
+
+
 } // namespace Node
 } // namespace Elastos
