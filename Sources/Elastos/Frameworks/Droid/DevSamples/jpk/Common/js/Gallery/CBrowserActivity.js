348fe985868d9c2be68456fbe241b1e16539bfc5
wanghuaimin <gdsys@126.com>
2016-06-28 23:04:45 +0800

    js epk : JSGallery--CBrowserActivity, Basic event handler

--- Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/Gallery/CBrowserActivity.js
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/Gallery/CBrowserActivity.js
@@ -11,12 +11,85 @@ module.exports = function(aoElastos, aoActivity){
 
     //member of CBrowserActivity
 
-    //private static const:
+    //private static const
     var TAG = "CBrowserActivity";
-    //private:
-    var mBackButton;
+    //var MSG_IMAGE_LOADED = 0;
+    //var MSG_LOAD_ALBUM_VIEW = 1;
 
-//----------------------------------------------------------
+    //private
+    var mShowBrowser = false;
+
+    var mShowPicPop = false;
+
+    var mBackButton;
+    var mAlbumName;
+    var mWallpaperLayout;
+    var mDetailLayout;
+    var mShareLayout;
+    var mPopButton;
+    var mGridView;
+
+    var mPicPopupWindow;
+    function ClosePicPopupWindow() {
+        if (mPicPopupWindow && mPicPopupWindow.IsShowing()) {
+            elog(TAG + "ClosePicPopupWindow()---");
+            mPicPopupWindow.Dismiss();
+            mPicPopupWindow = null;
+        }
+        mShowPicPop = false;
+    }
+    function OpenPicPopupWindow(v) {
+        elog(TAG + "OpenPicPopupWindow()-----");
+
+        var inflater = oActivity.GetSystemService(IContext__LAYOUT_INFLATER_SERVICE);
+        var layout = inflater.Inflate(R.layout.pic_popup_dialog);
+
+        var textTime = layout.FindViewById(R.id.pic_pop_time);
+        textTime.SetOnClickListener({
+            OnClick : function(aoView){
+                //
+            }
+        });
+
+        var textLocation = oActivity.FindViewById(R.id.pic_pop_location);
+        textLocation.SetOnClickListener({
+            OnClick : function(aoView){
+                //
+            }
+        });
+
+        mPicPopupWindow = Droid_New("CPopupWindow", layout,80, 62, true);
+        mPicPopupWindow.SetTouchable(true);
+        mPicPopupWindow.SetOutsideTouchable(true);
+
+        var res = aoActivity.GetResources();
+        var bitmapDrawable = Droid_New("CBitmapDrawable", res);
+
+        mPicPopupWindow.SetBackgroundDrawable(bitmapDrawable);
+        mPicPopupWindow.SetOnDismissListener({
+            OnDismiss : function () {
+                //
+            }
+        })
+
+        mPicPopupWindow.ShowAsDropDown(v);
+
+        mShowPicPop = true;
+    }
+
+    function GetSimpleAdapter() {
+        //TODO
+    }
+
+    var DataSourceHelper;
+
+    function PictureEntry() {
+        //TODO
+    }
+
+    var mPictureEntryList = []; //ArrayOf<PictureEntry>
+
+    //----------------------------------------------------------
 
     return {
         OnCreate:function(aoContext){
@@ -24,19 +97,97 @@ module.exports = function(aoElastos, aoActivity){
 
             oActivity.SetContentView(R.layout.activity_browser);
 
-            // mBackButton = oActivity.FindViewById(R.id.about_back);
-            // mBackButton.SetOnClickListener({
-            //     OnClick : function(aoView) {
-            //         oActivity.OnBackPressed();
-            //     },
-            // });
+            mBackButton = oActivity.FindViewById(R.id.pic_back);
+            mBackButton.SetOnClickListener({
+                OnClick : function(aoView) {
+                    if (mShowPicPop) {
+                        ClosePicPopupWindow();
+                    }
+                    else {
+                        oActivity.OnBackPressed();
+                    }
+                },
+            });
 
-            // var versionView = oActivity.FindViewById(R.id.about_version);
-            // var pkgName = oActivity.GetPackageName();
-            // var pkgMgr = oActivity.GetPackageManager();
-            // var pkgInfo = pkgMgr.GetPackageInfo(pkgName, 0);
-            // var pkgVersion = pkgInfo.GetVersionName();
-            // versionView.SetText(CString(pkgVersion));
+            mAlbumName = oActivity.FindViewById(R.id.pic_folder_name);
+
+            mWallpaperLayout = oActivity.FindViewById(R.id.pic_btn_wallpaper);
+            mWallpaperLayout.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_btn_wallpaper");
+               },
+            });
+
+            mDetailLayout = oActivity.FindViewById(R.id.pic_btn_detail);
+            mDetailLayout.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_btn_detail");
+               },
+            });
+
+            mShareLayout = oActivity.FindViewById(R.id.pic_btn_share);
+            mShareLayout.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_btn_share");
+               },
+            });
+
+            mPopButton = oActivity.FindViewById(R.id.pic_btn_popup);
+            mPopButton.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_btn_popup");
+                    if (mShowPicPop) {
+                        ClosePicPopupWindow();
+                    }
+                    else {
+                        OpenPicPopupWindow(v);
+                    }
+               },
+            });
+
+            mTime = oActivity.FindViewById(R.id.pic_pop_time);
+            mTime.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_pop_time");
+                    ClosePicPopupWindow();
+               },
+            });
+
+            mLocation = oActivity.FindViewById(R.id.pic_pop_location);
+            mLocation.SetOnClickListener({
+                OnClick : function(aoView) {
+                    elog(TAG + "OnClick()---pic_pop_location");
+                    ClosePicPopupWindow();
+               },
+            });
+
+            mGridView = FindViewById(R.id.pic_gridview);
+
+            var intent = oActivity.GetIntent();
+            if (intent) {
+                var path = intent.GetStringExtra(DataSourceHelper.SOURCE_PATH);
+                if (path.length) {
+                    folderName = intent.GetStringExtra(DataSourceHelper.SOURCE_DESC);
+                    var imageItems = DataSourceHelper.GetItemList(path);
+                    if (imageItems instanceof Array) {
+                        var entry;
+                        for (var i=0,im=imageItems.leng;i<im;i++) {
+                            entry = new PictureEntry();
+                            entry.sourcePath = path + DataSourceHelper.PATH_SPLITE + imageItems[i];
+                            entry.desc = folderName;
+                            elog(TAG + " > " + i + ", path:" + entry.sourcePath + ", folderName:" + entry.desc;
+                            mPictureEntryList.push(entry);
+                        }
+                        var cs = CString(folderName);
+                        mAlbumName.SetText(cs);
+                        mSimpleAdapter = GetSimpleAdapter();
+                        mGridView.SetAdapter(mSimpleAdapter);
+                    }
+                    else {
+                        elog(TAG + "imageItems is null!");
+                    }
+               }
+            }
         },  //OnCreate
         OnStart:function(aoContext){
             elog('====jso_activity_cb====OnStart.begin====');
