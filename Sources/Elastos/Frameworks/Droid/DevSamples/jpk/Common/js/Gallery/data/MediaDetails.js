88f96559f6b8667515415f563744ab9d97de3b6a
wanghuaimin <gdsys@126.com>
2016-07-07 00:32:07 +0800

    js epk : JSGallery--CPhotoActivity basic UI OK

--- Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/Gallery/data/MediaDetails.js
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/Gallery/data/MediaDetails.js
@@ -1,6 +1,248 @@
 module.exports = function(aoElastos){
+//--------common definition----begin----
+    var CObject = aoElastos.CObject;
 
-    var MediaDetails = {};
+    var CString = aoElastos.Core.CString;
+    var Droid_New = aoElastos.Droid.New;
+    var Core_New = aoElastos.Core.New;
+
+    var R = aoElastos.Application.R;
+//--------common definition----end----
+
+    var MediaDetails;
+
+//--------.h----begin----
+
+// #ifndef __MEDIADETAILS_H__
+// #define __MEDIADETAILS_H__
+
+// #include "app/Activity.h"
+// #include <elastos/HashMap.h>
+
+// using Elastos::Droid::Media::IExifInterface;
+// using Elastos::Utility::HashMap;
+
+// namespace Gallery {
+
+// class MediaDetails
+//     : public ElRefBase
+//     , public IInterface
+// {
+//     class FlashState
+//         : public ElRefBase
+//     {
+//     public:
+//         FlashState(
+//             /* [in] */ Int32 state);
+
+//         CARAPI_(Boolean) IsFlashFired();
+
+//     private:
+//         static Int32 FLASH_FIRED_MASK;
+//         static Int32 FLASH_RETURN_MASK;
+//         static Int32 FLASH_MODE_MASK;
+//         static Int32 FLASH_FUNCTION_MASK;
+//         static Int32 FLASH_RED_EYE_MASK;
+
+//     private:
+//         Int32 mState;
+//     };
+
+// public:
+//     static CARAPI_(void) ExtractExifInfo(
+//         /* [in] */ MediaDetails* details,
+//         /* [in] */ const String& filePath);
+
+// public:
+//     CAR_INTERFACE_DECL()
+
+//     CARAPI_(void) AddDetail(
+//         /* [in] */ Int32 index,
+//         /* [in] */ IInterface* value);
+
+//     CARAPI_(AutoPtr<IInterface>) GetDetail(
+//         /* [in] */ Int32 index);
+
+//     CARAPI_(Int32) Size();
+
+//     CARAPI_(void) SetUnit(
+//         /* [in] */ Int32 index,
+//         /* [in] */ Int32 unit);
+
+//     CARAPI_(Boolean) HasUnit(
+//         /* [in] */ Int32 index);
+
+//     CARAPI_(Int32) GetUnit(
+//         /* [in] */ Int32 index);
+
+// private:
+//     CARAPI_(void) SetExifData(
+//         /* [in] */ MediaDetails* details,
+//         /* [in] */ IExifInterface* exif,
+//         /* [in] */ const String& tag,
+//         /* [in] */ Int32 key);
+
+// public:
+//     static const Int32 INDEX_TITLE = 1;
+//     static const Int32 INDEX_DESCRIPTION = 2;
+//     static const Int32 INDEX_DATETIME = 3;
+//     static const Int32 INDEX_LOCATION = 4;
+//     static const Int32 INDEX_WIDTH = 5;
+//     static const Int32 INDEX_HEIGHT = 6;
+//     static const Int32 INDEX_ORIENTATION = 7;
+//     static const Int32 INDEX_DURATION = 8;
+//     static const Int32 INDEX_MIMETYPE = 9;
+//     static const Int32 INDEX_SIZE = 10;
+
+//     // for EXIF
+//     static const Int32 INDEX_MAKE = 100;
+//     static const Int32 INDEX_MODEL = 101;
+//     static const Int32 INDEX_FLASH = 102;
+//     static const Int32 INDEX_FOCAL_LENGTH = 103;
+//     static const Int32 INDEX_WHITE_BALANCE = 104;
+//     static const Int32 INDEX_APERTURE = 105;
+//     static const Int32 INDEX_SHUTTER_SPEED = 106;
+//     static const Int32 INDEX_EXPOSURE_TIME = 107;
+//     static const Int32 INDEX_ISO = 108;
+
+//     // Put this last because it may be long.
+//     static const Int32 INDEX_PATH = 200;
+
+//     static const String TAG;
+
+// private:
+//     // TreeMap<Integer, Object> mDetails;
+//     HashMap<Int32, AutoPtr<IInterface> > mDetails;
+//     HashMap<Int32, Int32> mUnits;
+
+// };
+
+// } // namespace Gallery
+
+// #endif // __MEDIADETAILS_H__
+
+//--------.h----end----
+
+//--------.cpp----begin----
+
+// #include "MediaDetails.h"
+// #include <elastos/Logger.h>
+// #include <elastos/StringBuilder.h>
+// #include <elastos/StringUtils.h>
+
+// using Elastos::Core::StringBuilder;
+// using Elastos::Core::StringUtils;
+// using Elastos::Utility::Logging::Logger;
+
+// namespace Gallery {
+
+// Int32 MediaDetails::FlashState::FLASH_FIRED_MASK = 1;
+// Int32 MediaDetails::FlashState::FLASH_RETURN_MASK = 2 | 4;
+// Int32 MediaDetails::FlashState::FLASH_MODE_MASK = 8 | 16;
+// Int32 MediaDetails::FlashState::FLASH_FUNCTION_MASK = 32;
+// Int32 MediaDetails::FlashState::FLASH_RED_EYE_MASK = 64;
+
+// // const Int32 MediaDetails::INDEX_TITLE = 1;
+// // const Int32 MediaDetails::INDEX_DESCRIPTION = 2;
+// // const Int32 MediaDetails::INDEX_DATETIME = 3;
+// // const Int32 MediaDetails::INDEX_LOCATION = 4;
+// // const Int32 MediaDetails::INDEX_WIDTH = 5;
+// // const Int32 MediaDetails::INDEX_HEIGHT = 6;
+// // const Int32 MediaDetails::INDEX_ORIENTATION = 7;
+// // const Int32 MediaDetails::INDEX_DURATION = 8;
+// // const Int32 MediaDetails::INDEX_MIMETYPE = 9;
+// // const Int32 MediaDetails::INDEX_SIZE = 10;
+
+// // const Int32 MediaDetails::INDEX_MAKE = 100;
+// // const Int32 MediaDetails::INDEX_MODEL = 101;
+// // const Int32 MediaDetails::INDEX_FLASH = 102;
+// // const Int32 MediaDetails::INDEX_FOCAL_LENGTH = 103;
+// // const Int32 MediaDetails::INDEX_WHITE_BALANCE = 104;
+// // const Int32 MediaDetails::INDEX_APERTURE = 105;
+// // const Int32 MediaDetails::INDEX_SHUTTER_SPEED = 106;
+// // const Int32 MediaDetails::INDEX_EXPOSURE_TIME = 107;
+// // const Int32 MediaDetails::INDEX_ISO = 108;
+
+// // const Int32 MediaDetails::INDEX_PATH = 200;
+
+// const String MediaDetails::TAG("MediaDetails");
+
+// MediaDetails::FlashState::FlashState(
+//     /* [in] */ Int32 state)
+//     : mState(state)
+// {}
+
+// Boolean MediaDetails::FlashState::IsFlashFired()
+// {
+//     return (mState & FLASH_FIRED_MASK) != 0;
+// }
+
+// void MediaDetails::ExtractExifInfo(
+//     /* [in] */ MediaDetails* details,
+//     /* [in] */  const String& filePath)
+// {}
+
+// CAR_INTERFACE_IMPL(MediaDetails, IInterface)
+
+// void MediaDetails::AddDetail(
+//     /* [in] */ Int32 index,
+//     /* [in] */ IInterface* value)
+// {
+//     mDetails.Insert(HashMap<Int32, AutoPtr<IInterface> >::ValueType(index, value));
+// }
+
+// AutoPtr<IInterface> MediaDetails::GetDetail(
+//     /* [in] */ Int32 index)
+// {
+//     HashMap<Int32, AutoPtr<IInterface> >::Iterator it = mDetails.Find(index);
+//     if (it != mDetails.End()) {
+//         return it->mSecond;
+//     }
+//     return NULL;
+// }
+
+// Int32 MediaDetails::Size()
+// {
+//     return mDetails.GetSize();
+// }
+
+// void MediaDetails::SetUnit(
+//     /* [in] */ Int32 index,
+//     /* [in] */ Int32 unit)
+// {
+//     mUnits.Insert(HashMap<Int32, Int32>::ValueType(index, unit));
+// }
+
+// Boolean MediaDetails::HasUnit(
+//     /* [in] */ Int32 index)
+// {
+//     HashMap<Int32, Int32 >::Iterator it = mUnits.Find(index);
+//     if (it != mUnits.End()) {
+//         return TRUE;
+//     }
+//     return FALSE;
+// }
+
+// Int32 MediaDetails::GetUnit(
+//     /* [in] */ Int32 index)
+// {
+//     HashMap<Int32, Int32 >::Iterator it = mUnits.Find(index);
+//     if (it != mUnits.End()) {
+//         return it->mSecond;
+//     }
+//     return 0;
+// }
+
+// void MediaDetails::SetExifData(
+//     /* [in] */ MediaDetails* details,
+//     /* [in] */ IExifInterface* exif,
+//     /* [in] */  const String& tag,
+//     /* [in] */ Int32 key)
+// {}
+
+// } // namespace Gallery
+
+//--------.cpp----end----
 
     return MediaDetails;
-};
+};  //exports
--- Sources/Elastos/Frameworks/Droid/DevSamples/jpk/Common/js/Gallery/util/z_empty.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(aoElastos){
-
-    var Xyz = {};
-
-    return Xyz;
-};
