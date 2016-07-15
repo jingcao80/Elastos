659064ded228e740b9abfa26473fa95ce8eac3ad
wanghuaimin <gdsys@126.com>
2016-07-16 02:23:38 +0800

    js epk : JSSettings main activity, compiled successfully, with i18n bug

--- Sources/Elastos/Frameworks/Droid/DevSamples/jpk/JSSettings/elastos/devsamples/node/jssettings/CNodeApp.cpp
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/JSSettings/elastos/devsamples/node/jssettings/CNodeApp.cpp
@@ -1,17 +1,2 @@
-#define JSActName CAlbumActivity
-#include "CNodeActivity_impl.h"
-
-#define JSActName CAboutActivity
-#include "CNodeActivity_impl.h"
-
-#define JSActName CBrowserActivity
-#include "CNodeActivity_impl.h"
-
-#define JSActName CPhotoActivity
-#include "CNodeActivity_impl.h"
-
-#define JSActName CEditActivity
-#include "CNodeActivity_impl.h"
-
-#define JSActName CWallpaperActivity
+#define JSActName CSettings
 #include "CNodeActivity_impl.h"
