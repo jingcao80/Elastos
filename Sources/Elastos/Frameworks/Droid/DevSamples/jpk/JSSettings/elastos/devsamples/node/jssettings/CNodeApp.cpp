139b15292a11d7a2748615f7efd09dc3582814e8
wanghuaimin <gdsys@126.com>
2016-07-15 18:07:17 +0800

    js epk : JSSettings basic frame

---
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/JSSettings/elastos/devsamples/node/jssettings/CNodeApp.cpp
@@ -0,0 +1,17 @@
+#define JSActName CAlbumActivity
+#include "CNodeActivity_impl.h"
+
+#define JSActName CAboutActivity
+#include "CNodeActivity_impl.h"
+
+#define JSActName CBrowserActivity
+#include "CNodeActivity_impl.h"
+
+#define JSActName CPhotoActivity
+#include "CNodeActivity_impl.h"
+
+#define JSActName CEditActivity
+#include "CNodeActivity_impl.h"
+
+#define JSActName CWallpaperActivity
+#include "CNodeActivity_impl.h"
---
+++ Sources/Elastos/Frameworks/Droid/DevSamples/jpk/JSSettings/elastos/devsamples/node/jssettings/CSettings.h
@@ -0,0 +1,5 @@
+#define JSActName CSettings
+#ifndef __CSettings_H__
+#define __CSettings_H__
+#include "CNodeActivity.h"
+#endif
