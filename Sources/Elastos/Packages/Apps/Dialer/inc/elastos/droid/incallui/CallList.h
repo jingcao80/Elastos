2a7af131914a1c2f669cc0a2454ef3532bc79941
shaojin <shao.jin@kortide.com>
2016-07-25 14:56:14 +0800

    porting incallui/calllist

--- Sources/Elastos/Packages/Apps/Dialer/inc/elastos/droid/incallui/CallList.h
+++ Sources/Elastos/Packages/Apps/Dialer/inc/elastos/droid/incallui/CallList.h
@@ -5,10 +5,20 @@
 #include "Elastos.Droid.Telecomm.h"
 #include "_Elastos.Droid.Dialer.h"
 #include "elastos/droid/incallui/Call.h"
+#include "elastos/droid/telecomm/telecom/Phone.h"
+#include "elastos/droid/os/Handler.h"
+#define HASH_FOR_TELECOMM_TELECOM
+#include "elastos/droid/ext/frameworkhash.h"
 #include <elastos/core/Object.h>
+#include <elastos/utility/etl/HashMap.h>
+#include <elastos/utility/etl/Set.h>
 
-using Elastos::Droid::Telecomm::Telecom::ICall;
+using Elastos::Droid::Os::Handler;
+using Elastos::Droid::Telecomm::Telecom::IPhone;
+using Elastos::Droid::Telecomm::Telecom::IPhoneListener;
 using Elastos::Core::Object;
+using Elastos::Utility::Etl::HashMap;
+using Elastos::Utility::Etl::Set;
 
 namespace Elastos {
 namespace Droid {
@@ -19,27 +29,268 @@ class CallList
     , public ICallList
     , public IInCallPhoneListener
 {
+private:
+    class PhoneListener
+        : public Phone::Listener
+    {
+    public:
+        PhoneListener(
+            /* [in] */ CallList* host)
+            : mHost(host)
+        {}
+
+        // @Override
+        CARAPI OnCallAdded(
+            /* [in] */ IPhone* phone,
+            /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* call);
+
+        // @Override
+        CARAPI OnCallRemoved(
+            /* [in] */ IPhone* phone,
+            /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* call);
+
+    private:
+        CallList* mHost;
+    };
+
+    class MyHandler : public Handler
+    {
+    public:
+        MyHandler(
+            /* [in] */ CallList* host)
+            : mHost(host)
+        {}
+
+        CARAPI HandleMessage(
+            /* [in] */ IMessage* msg);
+
+    private:
+        CallList* mHost;
+    };
+
 public:
-    CAR_INTERFACE_DECL();
+    CAR_INTERFACE_DECL()
 
     /**
      * Static singleton accessor method.
      */
     static CARAPI_(AutoPtr<CallList>) GetInstance();
 
-    CARAPI_(void) AddListener(
+    // @Override
+    CARAPI SetPhone(
+        /* [in] */ IPhone* phone);
+
+    // @Override
+    CARAPI ClearPhone();
+
+    /**
+     * Called when a single call disconnects.
+     */
+    CARAPI_(void) OnDisconnect(
+        /* [in] */ Call* call);
+
+    /**
+     * Called when a single call has changed.
+     */
+    CARAPI OnIncoming(
+        /* [in] */ Call* call,
+        /* [in] */ IList* textMessages);
+
+    /**
+     * Called when a single call has changed.
+     */
+    CARAPI_(void) OnUpdate(
+        /* [in] */ Call* call);
+
+    CARAPI_(void) NotifyCallUpdateListeners(
+        /* [in] */ Call* call);
+
+    /**
+     * Add a call update listener for a call id.
+     *
+     * @param callId The call id to get updates for.
+     * @param listener The listener to add.
+     */
+    CARAPI_(void) AddCallUpdateListener(
+        /* [in] */ const String& callId,
+        /* [in] */ ICallUpdateListener* listener);
+
+    /**
+     * Remove a call update listener for a call id.
+     *
+     * @param callId The call id to remove the listener for.
+     * @param listener The listener to remove.
+     */
+    CARAPI_(void) RemoveCallUpdateListener(
+        /* [in] */ const String& callId,
+        /* [in] */ ICallUpdateListener* listener);
+
+    CARAPI AddListener(
         /* [in] */ ICallListListener* listener);
+
+    CARAPI_(void) RemoveListener(
+        /* [in] */ ICallListListener* listener);
+
+    /**
+     * TODO: Change so that this function is not needed. Instead of assuming there is an active
+     * call, the code should rely on the status of a specific Call and allow the presenters to
+     * update the Call object when the active call changes.
+     */
+    CARAPI_(AutoPtr<Call>) GetIncomingOrActive();
+
+    CARAPI_(AutoPtr<Call>) GetOutgoingOrActive();
+
+    /**
+     * A call that is waiting for {@link PhoneAccount} selection
+     */
+    CARAPI_(AutoPtr<Call>) GetWaitingForAccountCall();
+
+    CARAPI_(AutoPtr<Call>) GetPendingOutgoingCall();
 
     CARAPI_(AutoPtr<Call>) GetOutgoingCall();
 
+    CARAPI_(AutoPtr<Call>) GetActiveCall();
+
+    CARAPI_(AutoPtr<Call>) GetBackgroundCall();
+
     CARAPI_(AutoPtr<Call>) GetDisconnectedCall();
+
+    CARAPI_(AutoPtr<Call>) GetDisconnectingCall();
+
+    CARAPI_(AutoPtr<Call>) GetSecondBackgroundCall();
 
     CARAPI_(AutoPtr<Call>) GetActiveOrBackgroundCall();
 
+    CARAPI_(AutoPtr<Call>) GetIncomingCall();
+
     CARAPI_(AutoPtr<Call>) GetFirstCall();
+
+    CARAPI_(Boolean) HasLiveCall();
+
+    /**
+     * Returns the first call found in the call map with the specified call modification state.
+     * @param state The session modification state to search for.
+     * @return The first call with the specified state.
+     */
+    CARAPI_(AutoPtr<Call>) GetVideoUpgradeRequestCall();
+
+    CARAPI_(AutoPtr<Call>) GetCallById(
+        /* [in] */ const String& callId);
 
     CARAPI_(AutoPtr<Call>) GetCallByTelecommCall(
         /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* telecommCall);
+
+    CARAPI_(AutoPtr<IList>) GetTextResponses(
+        /* [in] */ const String& callId);
+
+    /**
+     * Returns first call found in the call map with the specified state.
+     */
+    CARAPI_(AutoPtr<Call>) GetFirstCallWithState(
+        /* [in] */ Int32 state);
+
+    /**
+     * Returns the [position]th call found in the call map with the specified state.
+     * TODO: Improve this logic to sort by call time.
+     */
+    CARAPI_(AutoPtr<Call>) GetCallWithState(
+        /* [in] */ Int32 state,
+        /* [in] */ Int32 positionToFind);
+
+    /**
+     * This is called when the service disconnects, either expectedly or unexpectedly.
+     * For the expected case, it's because we have no calls left.  For the unexpected case,
+     * it is likely a crash of phone and we need to clean up our calls manually.  Without phone,
+     * there can be no active calls, so this is relatively safe thing to do.
+     */
+    CARAPI_(void) ClearOnDisconnect();
+
+    /**
+     * Notifies all video calls of a change in device orientation.
+     *
+     * @param rotation The new rotation angle (in degrees).
+     */
+    CARAPI_(void) NotifyCallsOfDeviceRotation(
+        /* [in] */ Int32 rotation);
+
+private:
+    /**
+     * Private constructor.  Instance should only be acquired through getInstance().
+     */
+    CallList();
+
+    static CARAPI_(AutoPtr<CallList>) InitInstance();
+
+    /**
+     * Processes an update for a single call.
+     *
+     * @param call The call to update.
+     */
+    CARAPI OnUpdateCall(
+        /* [in] */ Call* call);
+
+    /**
+     * Sends a generic notification to all listeners that something has changed.
+     * It is up to the listeners to call back to determine what changed.
+     */
+    CARAPI_(void) NotifyGenericListeners();
+
+    CARAPI_(void) NotifyListenersOfDisconnect(
+        /* [in] */ Call* call);
+
+    /**
+     * Updates the call entry in the local map.
+     * @return false if no call previously existed and no call was added, otherwise true.
+     */
+    CARAPI_(Boolean) UpdateCallInMap(
+        /* [in] */ Call* call);
+
+    CARAPI_(Int32) GetDelayForDisconnect(
+        /* [in] */ Call* call);
+
+    CARAPI UpdateCallTextMap(
+        /* [in] */ Call* call,
+        /* [in] */ IList* textResponses);
+
+    CARAPI_(Boolean) IsCallDead(
+        /* [in] */ Call* call);
+
+    /**
+     * Sets up a call for deletion and notifies listeners of change.
+     */
+    CARAPI_(void) FinishDisconnectedCall(
+        /* [in] */ Call* call);
+
+private:
+    static const Int32 DISCONNECTED_CALL_SHORT_TIMEOUT_MS = 200;
+    static const Int32 DISCONNECTED_CALL_MEDIUM_TIMEOUT_MS = 2000;
+    static const Int32 DISCONNECTED_CALL_LONG_TIMEOUT_MS = 5000;
+
+    static const Int32 EVENT_DISCONNECTED_TIMEOUT = 1;
+
+    static AutoPtr<CallList> sInstance;
+
+    HashMap<String, AutoPtr<Call> > mCallById;
+    HashMap<AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall>, AutoPtr<Call> > mCallByTelecommCall;
+    HashMap<String, AutoPtr<IList> > mCallTextReponsesMap;
+    /**
+     * ConcurrentHashMap constructor params: 8 is initial table size, 0.9f is
+     * load factor before resizing, 1 means we only expect a single thread to
+     * access the map so make only a single shard
+     */
+    Set<AutoPtr<ICallListListener> > mListeners;
+    HashMap<String, AutoPtr<List<AutoPtr<ICallUpdateListener> > > > mCallUpdateListenerMap;
+
+    AutoPtr<IPhone> mPhone;
+
+    AutoPtr<IPhoneListener> mPhoneListener;
+
+    /**
+     * Handles the timeout for destroying disconnected calls.
+     */
+    AutoPtr<IHandler> mHandler;
+
+    friend class PhoneListener;
 };
 
 } // namespace InCallUI
---
+++ Sources/Elastos/Packages/Apps/Dialer/src/elastos/droid/incallui/CallList.cpp
@@ -0,0 +1,581 @@
+
+#include "elastos/droid/incallui/CallList.h"
+#include "elastos/droid/incallui/Call.h"
+#include <elastos/utility/logging/Logger.h>
+
+using Elastos::Droid::InCallUI::EIID_ICallList;
+using Elastos::Droid::InCallUI::EIID_IInCallPhoneListener;
+using Elastos::Droid::Internal::Utility::IPreconditions;
+using Elastos::Droid::Internal::Utility::CPreconditions;
+using Elastos::Droid::Telecomm::Telecom::CDisconnectCause;
+
+using Elastos::Utility::Logging::Logger;
+
+namespace Elastos {
+namespace Droid {
+namespace InCallUI {
+
+//================================================================
+// CallList::PhoneListener
+//================================================================
+
+ECode CallList::PhoneListener::OnCallAdded(
+    /* [in] */ IPhone* phone,
+    /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* telecommCall)
+{
+    AutoPtr<Call> call = new Call(telecommCall);
+    if (call->GetState() == Call::State::INCOMING) {
+        mHost->OnIncoming(call, call->GetCannedSmsResponses());
+    }
+    else {
+        mHost->OnUpdate(call);
+    }
+    return NOERROR;
+}
+
+ECode CallList::PhoneListener::OnCallRemoved(
+    /* [in] */ IPhone* phone,
+    /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* telecommCall)
+{
+    HashMap<AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall>, AutoPtr<Call> >::Iterator it
+            = mHost->mCallByTelecommCall.Find(telecommCall);
+    if (it != mHost->mCallByTelecommCall.End()) {
+        AutoPtr<Call> call = it->mSecond;
+        call->SetState(Call::State::DISCONNECTED);
+        AutoPtr<IDisconnectCause> cause;
+        CDisconnectCause::New(IDisconnectCause::UNKNOWN, (IDisconnectCause**)&cause);
+        call->SetDisconnectCause(cause);
+        if (mHost->UpdateCallInMap(call)) {
+            Logger::W("CallList", "Removing call not previously disconnected %s", call->GetId().string());
+        }
+        return mHost->UpdateCallTextMap(call, NULL);
+    }
+    return NOERROR;
+}
+
+
+//================================================================
+// CallList::MyHandler
+//================================================================
+
+ECode CallList::MyHandler::HandleMessage(
+    /* [in] */ IMessage* msg)
+{
+    Int32 what;
+    msg->GetWhat(&what);
+    switch (what) {
+        case EVENT_DISCONNECTED_TIMEOUT: {
+            AutoPtr<IInterface> obj;
+            msg->GetObj((IInterface**)&obj);
+            Logger::D("CallList", "EVENT_DISCONNECTED_TIMEOUT %s", TO_CSTR(obj));
+            mHost->FinishDisconnectedCall((Call*)(ICall*)obj.Get());
+            break;
+        }
+        default:
+            Logger::D("CallList", "Message not expected: %d", what);
+            break;
+    }
+    return NOERROR;
+}
+
+
+//================================================================
+// CallList
+//================================================================
+
+const Int32 CallList::DISCONNECTED_CALL_SHORT_TIMEOUT_MS;
+const Int32 CallList::DISCONNECTED_CALL_MEDIUM_TIMEOUT_MS;
+const Int32 CallList::DISCONNECTED_CALL_LONG_TIMEOUT_MS;
+const Int32 CallList::EVENT_DISCONNECTED_TIMEOUT;
+
+AutoPtr<CallList> CallList::InitInstance()
+{
+    return new CallList();
+}
+AutoPtr<CallList> CallList::sInstance = CallList::InitInstance();
+
+CallList::CallList()
+{
+    mPhoneListener = new PhoneListener(this);
+    mHandler = new MyHandler(this);
+}
+
+CAR_INTERFACE_IMPL_2(CallList, Object, ICallList, IInCallPhoneListener);
+
+AutoPtr<CallList> CallList::GetInstance()
+{
+    return sInstance;
+}
+
+ECode CallList::SetPhone(
+    /* [in] */ IPhone* phone)
+{
+    mPhone = phone;
+    return mPhone->AddListener(mPhoneListener);
+}
+
+ECode CallList::ClearPhone()
+{
+    mPhone->RemoveListener(mPhoneListener);
+    mPhone = NULL;
+    return NOERROR;
+}
+
+void CallList::OnDisconnect(
+    /* [in] */ Call* call)
+{
+    if (UpdateCallInMap(call)) {
+        Logger::I("CallList", "onDisconnect: %s", TO_CSTR(call));
+        // notify those listening for changes on this specific change
+        NotifyCallUpdateListeners(call);
+        // notify those listening for all disconnects
+        NotifyListenersOfDisconnect(call);
+    }
+}
+
+ECode CallList::OnIncoming(
+    /* [in] */ Call* call,
+    /* [in] */ IList* textMessages)
+{
+    if (UpdateCallInMap(call)) {
+        Logger::I("CallList", "onIncoming - %s", TO_CSTR(call));
+    }
+    FAIL_RETURN(UpdateCallTextMap(call, textMessages))
+
+    Set<AutoPtr<ICallListListener> >::Iterator it = mListeners.Begin();
+    for (; it != mListeners.End(); ++it) {
+        (*it)->OnIncomingCall((ICall*)call);
+    }
+    return NOERROR
+}
+
+void CallList::OnUpdate(
+    /* [in] */ Call* call)
+{
+    OnUpdateCall(call);
+    NotifyGenericListeners();
+}
+
+void CallList::NotifyCallUpdateListeners(
+    /* [in] */ Call* call)
+{
+    HashMap<String, AutoPtr<List<AutoPtr<ICallUpdateListener> > > >::Iterator it
+            = mCallUpdateListenerMap.Find(call->GetId());
+    AutoPtr<List<AutoPtr<ICallUpdateListener> > > listeners;
+    if (it != mCallUpdateListenerMap.End()) {
+        listeners = it->mSecond;
+        List<AutoPtr<ICallUpdateListener> >::Iterator listenerIt = listeners->Begin();
+        for (; listenerIt != listeners->End(); ++listenerIt) {
+            (*listenerIt)->OnCallChanged((ICall*)call);
+        }
+    }
+}
+
+void CallList::AddCallUpdateListener(
+    /* [in] */ const String& callId,
+    /* [in] */ ICallUpdateListener* listener)
+{
+    HashMap<String, AutoPtr<List<AutoPtr<ICallUpdateListener> > > >::Iterator it
+            = mCallUpdateListenerMap.Find(callId);
+    AutoPtr<List<AutoPtr<ICallUpdateListener> > > listeners;
+    if (it == mCallUpdateListenerMap.End()) {
+        listeners = new List<AutoPtr<ICallUpdateListener> >();
+        mCallUpdateListenerMap[callId] = listeners;
+    }
+    else {
+        listeners = it->mSecond;
+    }
+    listeners->PushBack(listener);
+}
+
+void CallList::RemoveCallUpdateListener(
+    /* [in] */ const String& callId,
+    /* [in] */ ICallUpdateListener* listener)
+{
+    HashMap<String, AutoPtr<List<AutoPtr<ICallUpdateListener> > > >::Iterator it
+            = mCallUpdateListenerMap.Find(call->GetId());
+    AutoPtr<List<AutoPtr<ICallUpdateListener> > > listeners;
+    if (it != mCallUpdateListenerMap.End()) {
+        listeners = it->mSecond;
+        listeners->Remove(listener);
+    }
+}
+
+ECode CallList::AddListener(
+    /* [in] */ ICallListListener* listener)
+{
+    AutoPtr<IPreconditions> preconditions;
+    CPreconditions::AcquireSingleton((IPreconditions**)&preconditions);
+    FAIL_RETURN(preconditions->CheckNotNull(listener))
+
+    mListeners.Insert(listener);
+
+    // Let the listener know about the active calls immediately.
+    return listener->OnCallListChange((ICallList*)this);
+}
+
+void CallList::RemoveListener(
+    /* [in] */ ICallListListener* listener)
+{
+    if (listener != NULL) {
+        mListeners.Erase(listener);
+    }
+}
+
+AutoPtr<Call> CallList::GetIncomingOrActive()
+{
+    AutoPtr<Call> retval = GetIncomingCall();
+    if (retval == NULL) {
+        retval = GetActiveCall();
+    }
+    return retval;
+}
+
+AutoPtr<Call> CallList::GetOutgoingOrActive()
+{
+    AutoPtr<Call> retval = GetOutgoingCall();
+    if (retval == NULL) {
+        retval = GetActiveCall();
+    }
+    return retval;
+}
+
+AutoPtr<Call> CallList::GetWaitingForAccountCall()
+{
+    return GetFirstCallWithState(Call::State::PRE_DIAL_WAIT);
+}
+
+AutoPtr<Call> CallList::GetPendingOutgoingCall()
+{
+    return GetFirstCallWithState(Call::State::CONNECTING);
+}
+
+AutoPtr<Call> CallList::GetOutgoingCall()
+{
+    AutoPtr<Call> call = GetFirstCallWithState(Call::State::DIALING);
+    if (call == NULL) {
+        call = GetFirstCallWithState(Call::State::REDIALING);
+    }
+    return call;
+}
+
+AutoPtr<Call> CallList::GetActiveCall()
+{
+    return GetFirstCallWithState(Call::State::ACTIVE);
+}
+
+AutoPtr<Call> CallList::GetBackgroundCall()
+{
+    return GetFirstCallWithState(Call::State::ONHOLD);
+}
+
+AutoPtr<Call> CallList::GetDisconnectedCall()
+{
+    return GetFirstCallWithState(Call::State::DISCONNECTED);
+}
+
+AutoPtr<Call> CallList::GetDisconnectingCall()
+{
+    return GetFirstCallWithState(Call::State::DISCONNECTING);
+}
+
+AutoPtr<Call> CallList::GetSecondBackgroundCall()
+{
+    return GetCallWithState(Call::State::ONHOLD, 1);
+}
+
+AutoPtr<Call> CallList::GetActiveOrBackgroundCall()
+{
+    AutoPtr<Call> call = GetActiveCall();
+    if (call == NULL) {
+        call = GetBackgroundCall();
+    }
+    return call;
+}
+
+AutoPtr<Call> CallList::GetIncomingCall()
+{
+    AutoPtr<Call> call = GetFirstCallWithState(Call::State::INCOMING);
+    if (call == NULL) {
+        call = GetFirstCallWithState(Call::State::CALL_WAITING);
+    }
+
+    return call;
+}
+
+AutoPtr<Call> CallList::GetFirstCall()
+{
+    AutoPtr<Call> result = GetIncomingCall();
+    if (result == NULL) {
+        result = GetPendingOutgoingCall();
+    }
+    if (result == NULL) {
+        result = GetOutgoingCall();
+    }
+    if (result == NULL) {
+        result = GetFirstCallWithState(Call::State::ACTIVE);
+    }
+    if (result == NULL) {
+        result = GetDisconnectingCall();
+    }
+    if (result == NULL) {
+        result = GetDisconnectedCall();
+    }
+    return result;
+}
+
+Boolean CallList::HasLiveCall()
+{
+    AutoPtr<Call> call = GetFirstCall();
+    if (call == NULL) {
+        return FALSE;
+    }
+    return call != GetDisconnectingCall() && call != GetDisconnectedCall();
+}
+
+AutoPtr<Call> CallList::GetVideoUpgradeRequestCall()
+{
+    HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Begin();
+    for (; it != mCallById.End(); ++it) {
+        AutoPtr<Call> call = it->mSecond;
+        if (call->GetSessionModificationState() ==
+                Call::SessionModificationState::RECEIVED_UPGRADE_TO_VIDEO_REQUEST) {
+            return call;
+        }
+    }
+    return NULL;
+}
+
+AutoPtr<Call> CallList::GetCallById(
+    /* [in] */ const String& callId)
+{
+    AutoPtr<Call> call;
+    HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Find(callId);
+    if (it != mCallById.End()) call = it->mSecond;
+    return call;
+}
+
+AutoPtr<Call> CallList::GetCallByTelecommCall(
+    /* [in] */ Elastos::Droid::Telecomm::Telecom::ICall* telecommCall)
+{
+    AutoPtr<Call> call;
+    HashMap<AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall>, AutoPtr<Call> >::Iterator it
+            = mCallByTelecommCall.Find(telecommCall);
+    if (it != mCallByTelecommCall.End()) call = it->mSecond;
+    return call;
+}
+
+AutoPtr<IList> CallList::GetTextResponses(
+    /* [in] */ const String& callId)
+{
+    AutoPtr<List<String> > responses;
+    HashMap<String, AutoPtr<IList> >::Iterator it = mCallTextReponsesMap.Find(callId);
+    if (it != mCallTextReponsesMap.End()) responses = it->mSecond;
+    return responses;
+}
+
+AutoPtr<Call> CallList::GetFirstCallWithState(
+    /* [in] */ Int32 state)
+{
+    return GetCallWithState(state, 0);
+}
+
+AutoPtr<Call> CallList::GetCallWithState(
+    /* [in] */ Int32 state,
+    /* [in] */ Int32 positionToFind)
+{
+    AutoPtr<Call> retval;
+    Int32 position = 0;
+    HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Begin();
+    for (; it != mCallById.End(); ++it) {
+        AutoPtr<Call> call = it->mSecond;
+        if (call->GetState() == state) {
+            if (position >= positionToFind) {
+                retval = call;
+                break;
+            }
+            else {
+                position++;
+            }
+        }
+    }
+
+    return retval;
+}
+
+void CallList::ClearOnDisconnect()
+{
+    HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Begin();
+    for (; it != mCallById.End(); ++it) {
+        AutoPtr<Call> call = it->mSecond;
+        Int32 state = call->GetState();
+        if (state != Call::State::IDLE &&
+                state != Call::State::INVALID &&
+                state != Call::State::DISCONNECTED) {
+            call->SetState(Call::State::DISCONNECTED);
+            AutoPtr<IDisconnectCause> cause;
+            CDisconnectCause::New(IDisconnectCause::UNKNOWN, (IDisconnectCause**)&cause);
+            call->SetDisconnectCause(cause);
+            UpdateCallInMap(call);
+        }
+    }
+    NotifyGenericListeners();
+}
+
+ECode CallList::OnUpdateCall(
+    /* [in] */ Call* call)
+{
+    Logger::D("CallList", "\t%s", TO_CSTR(call));
+    if (UpdateCallInMap(call)) {
+        Logger::I("CallList", "onUpdate - %s", TO_CSTR(call));
+    }
+    FAIL_RETURN(UpdateCallTextMap(call, call->GetCannedSmsResponses()))
+    NotifyCallUpdateListeners(call);
+    return NOERROR;
+}
+
+void CallList::NotifyGenericListeners()
+{
+    Set<AutoPtr<ICallListListener> >::Iterator it = mListeners.Begin;
+    for (; it != mListeners.End(); ++it) {
+        (*it)->OnCallListChange((ICallList*)this);
+    }
+}
+
+void CallList::NotifyListenersOfDisconnect(
+    /* [in] */ Call* call)
+{
+    Set<AutoPtr<ICallListListener> >::Iterator it = mListeners.Begin;
+    for (; it != mListeners.End(); ++it) {
+        (*it)->OnDisconnect((ICallList*)this);
+    }
+}
+
+Boolean CallList::UpdateCallInMap(
+    /* [in] */ Call* call)
+{
+    AutoPtr<IPreconditions> preconditions;
+    CPreconditions::AcquireSingleton((IPreconditions**)&preconditions);
+    if (FAILED(preconditions->CheckNotNull((ICall*)call))) {
+        return FALSE;
+    }
+
+    Boolean updated = FALSE;
+
+    if (call->GetState() == Call::State::DISCONNECTED) {
+        // update existing (but do not add!!) disconnected calls
+        HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Find(call->GetId());
+        if (it != mCallById.End()) {
+
+            // For disconnected calls, we want to keep them alive for a few seconds so that the
+            // UI has a chance to display anything it needs when a call is disconnected.
+
+            // Set up a timer to destroy the call after X seconds.
+            AutoPtr<IMessage> msg;
+            mHandler->ObtainMessage(EVENT_DISCONNECTED_TIMEOUT, (ICall*)call, (IMessage**)&msg);
+            Boolean result
+            mHandler->SendMessageDelayed(msg, GetDelayForDisconnect(call), &result);
+
+            mCallById[call->GetId()] = call;
+            AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall> telecommCall = call->GetTelecommCall();
+            mCallByTelecommCall[telecommCall] = call;
+            updated = TRUE;
+        }
+    }
+    else if (!IsCallDead(call)) {
+        mCallById[call->GetId()] = call;
+        AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall> telecommCall = call->GetTelecommCall();
+        mCallByTelecommCall[telecommCall] = call;
+        updated = TRUE;
+    }
+    else if (mCallById.Find(call->GetId()) != mCallById.End()) {
+        mCallById.Erase(call->GetId());
+        AutoPtr<Elastos::Droid::Telecomm::Telecom::ICall> telecommCall = call->GetTelecommCall();
+        mCallByTelecommCall.Erase(telecommCall);
+        updated = TRUE;
+    }
+
+    return updated;
+}
+
+Int32 CallList::GetDelayForDisconnect(
+    /* [in] */ Call* call)
+{
+    AutoPtr<IPreconditions> preconditions;
+    CPreconditions::AcquireSingleton((IPreconditions**)&preconditions);
+    if (FAILED(preconditions->CheckState(call->getState() == Call::State::DISCONNECTED))) {
+        return 0;
+    }
+
+    Int32 cause;
+    call->GetDisconnectCause()->GetCode(&cause);
+    Int32 delay;
+    switch (cause) {
+        case IDisconnectCause::LOCAL:
+            delay = DISCONNECTED_CALL_SHORT_TIMEOUT_MS;
+            break;
+        case IDisconnectCause::REMOTE:
+            delay = DISCONNECTED_CALL_MEDIUM_TIMEOUT_MS;
+            break;
+        case IDisconnectCause::REJECTED:
+        case IDisconnectCause::MISSED:
+        case IDisconnectCause::CANCELED:
+            // no delay for missed/rejected incoming calls and canceled outgoing calls.
+            delay = 0;
+            break;
+        default:
+            delay = DISCONNECTED_CALL_LONG_TIMEOUT_MS;
+            break;
+    }
+
+    return delay;
+}
+
+ECode CallList::UpdateCallTextMap(
+    /* [in] */ Call* call,
+    /* [in] */ IList* textResponses)
+{
+    AutoPtr<IPreconditions> preconditions;
+    CPreconditions::AcquireSingleton((IPreconditions**)&preconditions);
+    FAIL_RETURN(preconditions->CheckNotNull((ICall*)call));
+
+    if (!IsCallDead(call)) {
+        if (textResponses != NULL) {
+            mCallTextReponsesMap[call->GetId()] = textResponses;
+        }
+    }
+    else if (mCallById.Find(call->GetId()) != mCallById.End()) {
+        mCallTextReponsesMap.Erase(call->GetId());
+    }
+}
+
+Boolean CallList::IsCallDead(
+    /* [in] */ Call* call)
+{
+    Int32 state = call.getState();
+    return Call::State::IDLE == state || Call::State::INVALID == state;
+}
+
+void CallList::FinishDisconnectedCall(
+    /* [in] */ Call* call)
+{
+    call->SetState(Call::State::IDLE);
+    UpdateCallInMap(call);
+    NotifyGenericListeners();
+}
+
+void CallList::NotifyCallsOfDeviceRotation(
+    /* [in] */ Int32 rotation)
+{
+    HashMap<String, AutoPtr<Call> >::Iterator it = mCallById.Begin();
+    for (; it != mCallById.End(); ++it) {
+        AutoPtr<Call> call = it->mSecond;
+        if (call->GetVideoCall() != NULL) {
+            call->GetVideoCall()->SetDeviceOrientation(rotation);
+        }
+    }
+}
+
+} // namespace InCallUI
+} // namespace Droid
+} // namespace Elastos
