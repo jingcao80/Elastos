2cdb6762bee1d7beb9e50a2301ebd1ad5ae1a84e
li.hui <li.hui@kortide.com>
2016-11-21 09:17:54 +0800

    fix code about securitydemo/CActivityOne

--- Sources/Elastos/Frameworks/Droid/DevSamples/SecurityDemo/src/elastos/devsamples/securitydemo/CActivityOne.cpp
+++ Sources/Elastos/Frameworks/Droid/DevSamples/SecurityDemo/src/elastos/devsamples/securitydemo/CActivityOne.cpp
@@ -976,15 +976,15 @@ ECode CActivityOne::ButtonElastosKeyPairGenerator()
 
     //return text->TestKeyPairGenerator_GenerateKeyPair_Encrypted_Success();                                   //OK
 
-    //return text->TestKeyPairGenerator_GenerateKeyPair_DSA_Unencrypted_Success();                                      //error
+    //return text->TestKeyPairGenerator_GenerateKeyPair_DSA_Unencrypted_Success();                             //OK
 
-    //return text->TestKeyPairGenerator_GenerateKeyPair_DSA_2048_Unencrypted_Success();                                 //error
+    //return text->TestKeyPairGenerator_GenerateKeyPair_DSA_2048_Unencrypted_Success();                        //OK
 
-    //return text->TestKeyPairGenerator_GenerateKeyPair_DSA_SpecifiedParams_Unencrypted_Success();                      //error
+    return text->TestKeyPairGenerator_GenerateKeyPair_DSA_SpecifiedParams_Unencrypted_Success();                      //error
 
-    return text->TestKeyPairGenerator_GenerateKeyPair_EC_Unencrypted_Success();                                //OK
+    //return text->TestKeyPairGenerator_GenerateKeyPair_EC_Unencrypted_Success();                              //OK
 
-    //return text->TestKeyPairGenerator_GenerateKeyPair_EC_P521_Unencrypted_Success();                                  //error
+    //return text->TestKeyPairGenerator_GenerateKeyPair_EC_P521_Unencrypted_Success();                         //OK
 
     //return text->TestKeyPairGenerator_GenerateKeyPair_RSA_Unencrypted_Success();                             //OK
 
@@ -999,7 +999,133 @@ ECode CActivityOne::ButtonElastosKeyStore()
 {
     AutoPtr<ElastosKeyStoreTest> test = new ElastosKeyStoreTest();
 
-    return test->TestKeyStore_Aliases_Encrypted_Success();
+    //return test->TestKeyStore_Aliases_Encrypted_Success();                                        //OK
+
+    //return test->TestKeyStore_Aliases_NotInitialized_Encrypted_Failure();                         //OK
+
+    //return test->TestKeyStore_ContainsAliases_PrivateAndCA_Encrypted_Success();                   //OK
+
+    //return test->TestKeyStore_ContainsAliases_CAOnly_Encrypted_Success();                         //OK
+
+    //return test->TestKeyStore_ContainsAliases_NonExistent_Encrypted_Failure();                    //OK
+
+    //return test->TestKeyStore_DeleteEntry_Encrypted_Success();                                    //OK
+
+    //return test->TestKeyStore_DeleteEntry_EmptyStore_Encrypted_Success();                         //OK
+
+    //return test->TestKeyStore_DeleteEntry_NonExistent_Encrypted_Success();                        //OK
+
+    //return test->TestKeyStore_GetCertificate_Single_Encrypted_Success();                                    //error
+
+    //return test->TestKeyStore_GetCertificate_NonExist_Encrypted_Failure();                        //OK
+
+    //return test->TestKeyStore_GetCertificateAlias_CAEntry_Encrypted_Success();                              //error
+
+    //return test->TestKeyStore_GetCertificateAlias_PrivateKeyEntry_Encrypted_Success();                      //error
+
+    //return test->TestKeyStore_GetCertificateAlias_CAEntry_WithPrivateKeyUsingCA_Encrypted_Success();        //error
+
+    //return test->TestKeyStore_GetCertificateAlias_NonExist_Empty_Encrypted_Failure();             //OK
+
+    //return test->TestKeyStore_GetCertificateAlias_NonExist_Encrypted_Failure();                   //OK
+
+    //return test->TestKeyStore_GetCertificateChain_SingleLength_Encrypted_Success();                         //error
+
+    //return test->TestKeyStore_GetCertificateChain_NonExist_Encrypted_Failure();                   //OK
+
+    //return test->TestKeyStore_GetCreationDate_PrivateKeyEntry_Encrypted_Success();                //OK
+
+    //return test->TestKeyStore_GetCreationDate_PrivateKeyEntry_Unencrypted_Success();              //OK
+
+    //return test->TestKeyStore_GetCreationDate_CAEntry_Encrypted_Success();                        //OK
+
+    //return test->TestKeyStore_GetEntry_NullParams_Encrypted_Success();                                      //error
+
+    //return test->TestKeyStore_GetEntry_DSA_NullParams_Unencrypted_Success();                                //error
+
+    //return test->TestKeyStore_GetEntry_EC_NullParams_Unencrypted_Success();                                 //error
+
+    //return test->TestKeyStore_GetEntry_RSA_NullParams_Unencrypted_Success();                                //error
+
+    //return test->TestKeyStore_GetEntry_Nonexistent_NullParams_Encrypted_Failure();                //OK
+
+    //return test->TestKeyStore_GetEntry_Nonexistent_NullParams_Unencrypted_Failure();              //OK
+
+    //return test->TestKeyStore_GetKey_NoPassword_Encrypted_Success();                              //OK
+
+    //return test->TestKeyStore_GetKey_NoPassword_Unencrypted_Success();                            //OK
+
+    //return test->TestKeyStore_GetKey_Certificate_Encrypted_Failure();                             //OK
+
+    //return test->TestKeyStore_GetKey_NonExistent_Encrypted_Failure();                             //OK
+
+    //return test->TestKeyStore_GetProvider_Encrypted_Success();                                    //OK
+
+    //return test->TestKeyStore_GetType_Encrypted_Success();                                        //OK
+
+    //return test->TestKeyStore_IsCertificateEntry_CA_Encrypted_Success();                          //OK
+
+    //return test->TestKeyStore_IsCertificateEntry_PrivateKey_Encrypted_Failure();                  //OK
+
+    //return test->TestKeyStore_IsCertificateEntry_NonExist_Encrypted_Failure();                    //OK
+
+    //return test->TestKeyStore_IsCertificateEntry_NonExist_Unencrypted_Failure();                  //OK
+
+    //return test->TestKeyStore_IsKeyEntry_PrivateKey_Encrypted_Success();                          //OK
+
+    //return test->TestKeyStore_IsKeyEntry_CA_Encrypted_Failure();                                  //OK
+
+    //return test->TestKeyStore_IsKeyEntry_NonExist_Encrypted_Failure();                            //OK
+
+    //return test->TestKeyStore_SetCertificate_CA_Encrypted_Success();                                         //error
+
+    //return test->TestKeyStore_SetCertificate_CAExists_Overwrite_Encrypted_Success();              //OK
+
+    //return test->TestKeyStore_SetCertificate_PrivateKeyExists_Encrypted_Failure();                //OK
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_Encrypted_Success();                                  //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_DSA_Unencrypted_Success();                            //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_EC_Unencrypted_Success();                             //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_RSA_Unencrypted_Success();                            //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_Params_Unencrypted_Failure();                         //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_Overwrites_PrivateKeyEntry_Encrypted_Success();       //error
+
+    //return test->TestKeyStore_SetEntry_CAEntry_Overwrites_PrivateKeyEntry_Encrypted_Success();               //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_Overwrites_CAEntry_Encrypted_Success();               //error
+
+    //return test->TestKeyStore_SetEntry_PrivateKeyEntry_Overwrites_ShortPrivateKeyEntry_Encrypted_Success();  //error
+
+    //return test->TestKeyStore_SetEntry_CAEntry_Overwrites_CAEntry_Encrypted_Success();                       //error
+
+    //return test->TestKeyStore_SetKeyEntry_ProtectedKey_Encrypted_Failure();                      //OK
+
+    //return test->TestKeyStore_SetKeyEntry_Encrypted_Success();                                               //error
+
+    //return test->TestKeyStore_SetKeyEntry_Replaced_Encrypted_Success();                                      //error
+
+    //return test->TestKeyStore_SetKeyEntry_ReplacedChain_Encrypted_Success();                                 //error
+
+    //return test->TestKeyStore_SetKeyEntry_ReplacedChain_DifferentPrivateKey_Encrypted_Failure();             //error
+
+    //return test->TestKeyStore_SetKeyEntry_ReplacedChain_UnencryptedToEncrypted_Failure();                    //error
+
+    //return test->TestKeyStore_Size_Encrypted_Success();                                          //OK
+
+    //return test->TestKeyStore_Store_LoadStoreParam_Encrypted_Failure();                          //OK
+
+    //eturn test->TestKeyStore_Load_InputStreamSupplied_Encrypted_Failure();                                   //error
+
+    //return test->TestKeyStore_Load_PasswordSupplied_Encrypted_Failure();                         //OK
+
+    //return test->TestKeyStore_Store_OutputStream_Encrypted_Failure();                            //OK
+
+    return test->TestKeyStore_KeyOperations_Wrap_Encrypted_Success();                                          //error
 }
 
 ECode CActivityOne::SignatureTest()
